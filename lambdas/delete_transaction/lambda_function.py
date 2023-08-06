b7b07aaf9171ec81f460c59e7f82b58500234870
Peter <davison.pl@gmail.com>
2023-08-06 13:50:13 -0400

    Add SNS trigger for delete

--- lambdas/delete_transaction/lambda_function.py
+++ lambdas/delete_transaction/lambda_function.py
@@ -31,17 +31,32 @@ def delete_transaction(id_str):
 
 def lambda_handler(event, context):
     """
-    DELETE request, params in event["queryStringParameters"]
-
+    - API DELETE request params in event["queryStringParameters"]
+    - SNS topic: parse event["Records"][i]["Sns"]["Message"]
+    
     Only care about status code returned by API to client
     """
-    id_str = event["queryStringParameters"]["id"]
+    if "queryStringParameters" in event:
+        print("Deleting from API trigger")
+        id_str = event["queryStringParameters"]["id"]
+        delete_transaction(id_str)
 
-    delete_transaction(id_str)
+    elif "Records" in event:
+        print("Deleting from SNS trigger")
+        transactions = []
+        for msg in event["Records"]:
+            transactions.extend(json.loads(msg["Sns"]["Message"]))
+
+        for t in transactions:
+            delete_transaction(t["id"])
+
+    else:
+        print("ERROR: unknown trigger event format")
+        print("Lambda Event:")
+        print(event)
 
     return {"statusCode": 200}
    
 
-# Running locally assumes month-category query 
 if __name__ == "__main__":
     print("Edit not implemented locally")
